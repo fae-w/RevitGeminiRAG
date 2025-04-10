fef88d0eb41b730386d8813935344a32d439ca63
Ismail Seleit <ismailseleit@gmail.com>
2025-04-10 12:26:55 +0100

    Initial commit with .gitignore and project files

---
+++ PromptForm.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace RevitGeminiRAG
+{
+    partial class Gemini_RAG
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.promptTextBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Artifakt Element Medium", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(22, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(413, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter your Revit API question/task:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // promptTextBox
+            // 
+            this.promptTextBox.AccessibleName = "promptTextBox";
+            this.promptTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.promptTextBox.Font = new System.Drawing.Font("Artifakt Element Medium", 10.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.promptTextBox.Location = new System.Drawing.Point(0, 0);
+            this.promptTextBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.promptTextBox.Multiline = true;
+            this.promptTextBox.Name = "promptTextBox";
+            this.promptTextBox.Size = new System.Drawing.Size(834, 329);
+            this.promptTextBox.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.AccessibleName = "submitButton";
+            this.button1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.button1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.button1.Font = new System.Drawing.Font("Artifakt Element Medium", 10.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(0, 249);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(834, 80);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "submit";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // Gemini_RAG
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(834, 329);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.promptTextBox);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Artifakt Element Medium", 4.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Gemini_RAG";
+            this.Text = "Gemini RAG";
+            this.Load += new System.EventHandler(this.PromptForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox promptTextBox;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ PromptForm.cs
@@ -0,0 +1,90 @@
+﻿using System;
+// Make sure System.Windows.Forms is referenced in your project
+// and included here. Visual Studio usually adds this automatically
+// when you add a Form.
+using System.Windows.Forms;
+
+namespace RevitGeminiRAG
+{
+    // The 'partial' keyword means the other part of this class definition
+    // is in PromptForm.Designer.cs (where InitializeComponent and control definitions live)
+    public partial class Gemini_RAG : Form
+    {
+        // Constructor - called when you create an instance of the form
+        public Gemini_RAG()
+        {
+            // This method is defined in PromptForm.Designer.cs
+            // and sets up all the controls you added in the visual designer.
+            InitializeComponent();
+
+            // Optional: Set initial focus to the text box when the form loads
+            this.Load += (s, e) => { promptTextBox.Focus(); };
+        }
+
+        /// <summary>
+        /// Public property to safely get the text entered by the user
+        /// into the promptTextBox control.
+        /// </summary>
+        public string UserPrompt
+        {
+            // The 'get' accessor returns the current value of the Text property
+            // of the control named 'promptTextBox'.
+            // Make sure the TextBox control on your form is actually named "promptTextBox"
+            // in the Properties window of the designer.
+            get { return promptTextBox.Text; }
+
+            // You could add a 'set' accessor if you wanted to pre-fill the box,
+            // but it's not needed for just retrieving the user's input.
+            // set { promptTextBox.Text = value; }
+        }
+
+        // --- Optional Event Handlers ---
+
+        // This handler likely got created if you double-clicked the label in the designer.
+        // If it's empty, you can safely remove it (and remove the linkage
+        // in PromptForm.Designer.cs if you're comfortable doing that, otherwise leave it).
+        private void label1_Click(object sender, EventArgs e)
+        {
+            // Nothing needed here typically for a static label.
+        }
+
+        // This handler runs when the form first loads.
+        private void PromptForm_Load(object sender, EventArgs e)
+        {
+            // This is a good place for setup code that needs to run
+            // after InitializeComponent(), like setting focus as shown
+            // in the constructor example above, or perhaps loading saved settings.
+        }
+
+        // Example of handling the Submit button click for validation (Optional)
+        // You would need to connect this method to the Submit button's Click event
+        // in the form designer's Properties window (Events section - lightning bolt icon).
+        // Note: Setting the button's DialogResult property often handles closing automatically.
+        /*
+        private void submitButton_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(promptTextBox.Text))
+            {
+                MessageBox.Show("Please enter a prompt before submitting.",
+                                "Input Required",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Warning);
+                // Important: Prevent the form from closing if validation fails
+                // when DialogResult is set on the button.
+                this.DialogResult = DialogResult.None;
+            }
+            else
+            {
+                // Ensure DialogResult is set correctly if validation passes
+                // (especially if you overrode it with DialogResult.None above)
+                // This assumes the button's DialogResult property was set to OK in the designer.
+                 if(this.DialogResult == DialogResult.None) // Only set if validation previously failed
+                 {
+                    this.DialogResult = DialogResult.OK;
+                 }
+                // No need to call this.Close() explicitly if DialogResult is set correctly.
+            }
+        }
+        */
+    }
+}
